f184c9adc0d2091bf95fe0b99a9c32e859f1f27c
Mark <MarkKranenburg@hotmail.nl>
2024-02-24 19:14:33 +0100

    add header buttons and header grid

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ let isDarkThemeOn = false;
 
 const changeThemeBtn = document.querySelector('#changeThemeBtn');
 const body = document.querySelector('body');
+const header = document.querySelector('header')
 
 changeThemeBtn.addEventListener('click', () => {
     if (isDarkThemeOn === false) {
         body.classList.remove('lightThemeBody');
         body.classList.add('darkThemeBody');
+        header.classList.remove('lightThemeHeader');
+        header.classList.add('darkThemeHeader');
         isDarkThemeOn = true;
     } else {
         body.classList.remove('darkThemeBody');
         body.classList.add('lightThemeBody');
+        header.classList.remove('darkThemeHeader');
+        header.classList.add('lightThemeHeader');
         isDarkThemeOn = false;
     }
 })
