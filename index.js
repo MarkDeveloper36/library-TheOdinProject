e97dc0ddcbb6a85bbcca25ac7fbe192422a515bf
Mark <MarkKranenburg@hotmail.nl>
2024-02-27 08:32:43 +0100

    add theme changing colors to buttons and text

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ let isDarkThemeOn = false;
 
 const changeThemeBtn = document.querySelector('#changeThemeBtn');
 const body = document.querySelector('body');
-const header = document.querySelector('header')
+const header = document.querySelector('header');
+const btns = document.querySelectorAll('button');
+const logo = document.querySelector('svg');
 
 changeThemeBtn.addEventListener('click', () => {
     if (isDarkThemeOn === false) {
@@ -10,12 +12,22 @@ changeThemeBtn.addEventListener('click', () => {
         body.classList.add('darkThemeBody');
         header.classList.remove('lightThemeHeader');
         header.classList.add('darkThemeHeader');
+        btns.forEach(btn => {
+            btn.classList.remove('lightThemeBtn');
+            btn.classList.add('darkThemeBtn');
+        });
+        logo.classList.add('darkThemeSvg');
         isDarkThemeOn = true;
     } else {
         body.classList.remove('darkThemeBody');
         body.classList.add('lightThemeBody');
         header.classList.remove('darkThemeHeader');
         header.classList.add('lightThemeHeader');
+        btns.forEach(btn => {
+            btn.classList.remove('darkThemeBtn');
+            btn.classList.add('lightThemeBtn');
+        });
+        logo.classList.remove('darkThemeSvg');
         isDarkThemeOn = false;
     }
 })
