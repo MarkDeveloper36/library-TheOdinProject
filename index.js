4137660231c6f4ae28c1b3b0ecd0feb297b32958
Mark <MarkKranenburg@hotmail.nl>
2024-02-27 13:02:13 +0100

    add some ligic to display cards

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const library = [];
 let isDarkThemeOn = false;
 
 const changeThemeBtn = document.querySelector('#changeThemeBtn');
@@ -6,8 +7,77 @@ const header = document.querySelector('header');
 const btns = document.querySelectorAll('button');
 const svgs = document.querySelectorAll('svg');
 const footer = document.querySelector('footer');
+const addBookBtn = document.querySelector('#addBookBtn');
+
+const cardSoundEffect = new Audio('Drawing Playing Cards Sound Effect.mp3');
+
+function Book(title, author, totPages, haveFinished) {
+    this.title = title;
+    this.author = author;
+    this.totPages = totPages;
+    this.haveFinished = haveFinished
+}
+
+const testBook = new Book('shoeDog', 'Phil Knight', 413, 'not yet');
+const testBook2 = new Book('shoeDog2', 'Phil Knight2', 13, 'Yes');
+library.push(testBook);
+library.push(testBook2);
+
+displayBooks();
+
+addBookBtn.addEventListener('click', () => {
+    let title = prompt('book title', 'harry porrter');
+    let author = prompt('author');
+    let totPages = prompt('pages');
+    let haveFinished = prompt('finished?');
+    let newBook = new Book(title, author, totPages, haveFinished);
+    library.push(newBook);
+    displayBooks();
+})
+
+
+
+function displayBooks() {
+    let currentCars = main.querySelectorAll('div');
+    currentCars.forEach(card => {
+        console.log(card);
+    })
+    for(let i = 0; i < library.length; i++) {
+        let card = document.createElement('div');
+        giveStyleAndTheme(card);
+        card.textContent = library[i].title;
+        main.appendChild(card);
+    }
+}
+
+function giveStyleAndTheme(card) {
+    card.classList.add('bookCard');
+    if (isDarkThemeOn === false) {
+        card.classList.add('lightThemeCard');
+    } else {
+        card.classList.add('darkThemeCard');
+    }
+    card.addEventListener('mouseenter', playSoundEffect);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 changeThemeBtn.addEventListener('click', () => {
+    let cards = document.querySelectorAll('.bookCard');
     if (isDarkThemeOn === false) {
         body.classList.remove('lightThemeBody');
         body.classList.add('darkThemeBody');
@@ -22,6 +92,10 @@ changeThemeBtn.addEventListener('click', () => {
         svgs.forEach(svg => {
             svg.classList.add('darkThemeSvg');
         });
+        cards.forEach(card => {
+            card.classList.remove('lightThemeCard');
+            card.classList.add('darkThemeCard');
+        })
         isDarkThemeOn = true;
     } else {
         body.classList.remove('darkThemeBody');
@@ -37,6 +111,15 @@ changeThemeBtn.addEventListener('click', () => {
         svgs.forEach(svg => {
             svg.classList.remove('darkThemeSvg');
         });
+        cards.forEach(card => {
+            card.classList.remove('darkThemeCard');
+            card.classList.add('lightThemeCard');
+        })
         isDarkThemeOn = false;
     }
-})
+});
+
+function playSoundEffect() {
+    cardSoundEffect.currentTime = 5.6;
+    cardSoundEffect.play();
+}
